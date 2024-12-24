2d502d2709969a54f202f220aa2640f711d8933b
Brian <26396307+wtLau@users.noreply.github.com>
2024-12-24 15:04:16 -0800

    fix: disable newsletter

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,117 +1,118 @@
 export type Image = {
-    src: string;
-    alt?: string;
-    caption?: string;
+	src: string;
+	alt?: string;
+	caption?: string;
 };
 
 export type Link = {
-    text: string;
-    href: string;
+	text: string;
+	href: string;
 };
 
 export type Hero = {
-    title?: string;
-    text?: string;
-    image?: Image;
-    actions?: Link[];
+	title?: string;
+	text?: string;
+	image?: Image;
+	actions?: Link[];
 };
 
 export type Subscribe = {
-    title?: string;
-    text?: string;
-    formUrl: string;
+	title?: string;
+	text?: string;
+	formUrl?: string;
 };
 
 export type SiteConfig = {
-    logo?: Image;
-    title: string;
-    subtitle?: string;
-    description: string;
-    image?: Image;
-    headerNavLinks?: Link[];
-    footerNavLinks?: Link[];
-    socialLinks?: Link[];
-    hero?: Hero;
-    subscribe?: Subscribe;
-    postsPerPage?: number;
-    projectsPerPage?: number;
+	logo?: Image;
+	title: string;
+	subtitle?: string;
+	description: string;
+	image?: Image;
+	headerNavLinks?: Link[];
+	footerNavLinks?: Link[];
+	socialLinks?: Link[];
+	hero?: Hero;
+	subscribe?: Subscribe;
+	postsPerPage?: number;
+	projectsPerPage?: number;
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Brian Lau',
-    subtitle: 'Personal blog website built with Astro.js',
-    description: 'Brian share thoughts about software development, front-end, programming and learnings',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
-    headerNavLinks: [
-        {
-            text: 'Home',
-            href: '/'
-        },
-        // {
-        //     text: 'Projects',
-        //     href: '/projects'
-        // },
-        {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'Tags',
-            href: '/tags'
-        }
-    ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-    ],
-    socialLinks: [
-        {
-            text: 'LinkedIn',
-            href: 'https://www.linkedin.com/in/brian-lau'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://www.instagram.com/brian.laulau/'
-        },
-        {
-            text: 'Github',
-            href: 'https://github.com/wtlau'
-        }
-    ],
-    hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Brian Lau**, a web developer at Arcteryx, dedicated to the realms of software engineering and frontend development. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/wtlau'>GitHub</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
-    },
-    subscribe: {
-        title: 'Subscribe to Brian Newsletter',
-        text: 'One update per year. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
-    postsPerPage: 20,
-    projectsPerPage: 8
+	title: "Brian Lau",
+	subtitle: "Personal blog website built with Astro.js",
+	description:
+		"Brian share thoughts about software development, front-end, programming and learnings",
+	image: {
+		src: "/dante-preview.jpg",
+		alt: "Dante - Astro.js and Tailwind CSS theme",
+	},
+	headerNavLinks: [
+		{
+			text: "Home",
+			href: "/",
+		},
+		// {
+		//     text: 'Projects',
+		//     href: '/projects'
+		// },
+		{
+			text: "Blog",
+			href: "/blog",
+		},
+		{
+			text: "Tags",
+			href: "/tags",
+		},
+	],
+	footerNavLinks: [
+		{
+			text: "About",
+			href: "/about",
+		},
+		{
+			text: "Contact",
+			href: "/contact",
+		},
+		{
+			text: "Terms",
+			href: "/terms",
+		},
+	],
+	socialLinks: [
+		{
+			text: "LinkedIn",
+			href: "https://www.linkedin.com/in/brian-lau",
+		},
+		{
+			text: "Instagram",
+			href: "https://www.instagram.com/brian.laulau/",
+		},
+		{
+			text: "Github",
+			href: "https://github.com/wtlau",
+		},
+	],
+	hero: {
+		title: "Hi There & Welcome to My Corner of the Web!",
+		text: "I'm **Brian Lau**, a web developer at Arcteryx, dedicated to the realms of software engineering and frontend development. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/wtlau'>GitHub</a>.",
+		image: {
+			src: "/hero.jpeg",
+			alt: "A person sitting at a desk in front of a computer",
+		},
+		actions: [
+			{
+				text: "Get in Touch",
+				href: "/contact",
+			},
+		],
+	},
+	subscribe: {
+		title: "Subscribe to Brian Newsletter",
+		text: "One update per year. All the latest posts directly in your inbox.",
+		// formUrl: '#'
+	},
+	postsPerPage: 20,
+	projectsPerPage: 8,
 };
 
 export default siteConfig;
